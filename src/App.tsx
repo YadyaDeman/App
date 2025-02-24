8edbd4c3927e0a9be72b19008fc1c647165fe20b
Yadviha <demyanovaya@gmail.com>
2025-02-24 21:31:26 +0100

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,11 @@
-import React, { useState } from "react";
+import Home from './pages/UserApp/Home/Home';
 
-const App = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    // Логика авторизации
-    console.log("Username:", username);
-    console.log("Password:", password);
-  };
-
+function App() {
   return (
-    <div style={{ maxWidth: 400, margin: "0 auto", padding: 20 }}>
-      <h2>Авторизация</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Логин:</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Пароль:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Войти</button>
-      </form>
+    <div>
+      <Home />
     </div>
   );
-};
+}
 
 export default App;
